a77375ecc82b7ec78b2abff22eb372465f2a8de0
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-27 19:38:52 +0200

    small prime method

---
+++ prime.rb
@@ -0,0 +1,17 @@
+def prime?(num)
+  # TO DO: return true for prime numbers: 2, 3, 5, 7....
+  if num == 1 then return false end
+  (2..(num - 1)).each do |nr|
+    if (num % nr).zero?
+      return false
+    end
+  end
+  true
+end
+
+p prime?(1) # false
+p prime?(3) # true
+p prime?(4) # false
+p prime?(5) # true
+p prime?(6) # false
+p prime?(7) # true
